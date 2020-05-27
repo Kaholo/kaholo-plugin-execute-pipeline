50d25433ab6e9b11f111f8bb8401f2ccaf491d63
Matan Kadosh <kadosh10001@gmail.com>
2020-05-27 12:07:28 +0300

    * add support fort agents allowing both array or comma separated list * validate agents field * add support for all agents versions * bump version to 1.5.0 * update zip

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
 const request = require("request");
-const env = require('../../../core/src/environment/environment');
 
 function executeMap(action,settings) {
+    const serverUrl = getServerUrl();
     return new Promise((resolve, reject) => {
-        let executionUrl = `${env.server_url}/api/maps/${action.params.MAP}/execute/`;
+        let executionUrl = `${serverUrl}/api/maps/${action.params.MAP}/execute/`;
         if (action.params.VERSION) {
             executionUrl += action.params.VERSION;
+        }
+
+        let agents;
+        if(action.params.AGENTS){
+            if (typeof action.params.AGENTS == 'string'){
+                agents = action.params.AGENTS.split(',').map(agent=>agent.trim());
+            } else if (Array.isArray(action.params.AGENTS)) {
+                agents = action.params.AGENTS;
+            } else {
+                return reject("Agents parameter must be either an Array or comma seperated list");
+            }
         }
 
         request.post(executionUrl, {
             body: {
                 trigger: (action.params.TRIGGER || 'Started by map-executer plugin'),
-                agents: action.params.AGENTS,
+                agents: agents,
                 mergeConfig: action.params.MERGE_CONFIG,
                 config: action.params.CONFIG
             },
@@ -29,6 +40,22 @@ function executeMap(action,settings) {
         });
 
     });
+}
+
+/**
+ * In order to be able to support multiple agent versions (<1.3.0, >=1.3.0)
+ */
+function getServerUrl(){
+    let server_url;
+    try{
+        const env = require('../../../core/src/environment/environment');
+        server_url = env.server_url
+    } catch(err) {
+        server_url = process.env.SERVER_URL;
+    }
+
+    if(server_url) return server_url;
+    throw "Could not determine Kaholo server URL";
 }
 
 module.exports = {
