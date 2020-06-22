41a5b5367711694a7ce44f242e1e5d6a112b14ef
Matan Kadosh <kadosh10001@gmail.com>
2020-06-22 18:02:55 +0300

    * add .gitignore * remove version and mergeConfig params * fix sending empty params * bump version to 2.0.0 * update zip

--- src/app.js
+++ src/app.js
@@ -1,63 +1,69 @@
 const request = require("request");
 
-function executeMap(action,settings) {
-    const serverUrl = getServerUrl();
-    return new Promise((resolve, reject) => {
-        let executionUrl = `${serverUrl}/api/maps/${action.params.MAP}/execute/`;
-        if (action.params.VERSION) {
-            executionUrl += action.params.VERSION;
+function executeMap(action, settings) {
+  const serverUrl = getServerUrl();
+  return new Promise((resolve, reject) => {
+    let executionUrl = `${serverUrl}/api/maps/${action.params.MAP}/execute/`;
+    
+    let agents = [];
+    if (action.params.AGENTS) {
+      if (typeof action.params.AGENTS == "string") {
+        agents = action.params.AGENTS.split(",").map((agent) => agent.trim());
+      } else if (Array.isArray(action.params.AGENTS)) {
+        agents = action.params.AGENTS;
+      } else {
+        return reject(
+          "Agents parameter must be either an Array or comma seperated list"
+        );
+      }
+    }
+
+    const body = {
+      trigger: action.params.TRIGGER || "Started by map-executer plugin",
+      agents: agents
+    }
+
+    if(action.params.CONFIG){
+      body.config = action.params.CONFIG
+    }
+
+    request.post(
+      executionUrl,
+      {
+        body: body,
+        headers: {
+          authorization: settings.TOKEN,
+        },
+        json: true,
+      },
+      function (error, response, body) {
+        if (error || response.statusCode !== 200) {
+          return reject(body || error);
         }
-
-        let agents;
-        if(action.params.AGENTS){
-            if (typeof action.params.AGENTS == 'string'){
-                agents = action.params.AGENTS.split(',').map(agent=>agent.trim());
-            } else if (Array.isArray(action.params.AGENTS)) {
-                agents = action.params.AGENTS;
-            } else {
-                return reject("Agents parameter must be either an Array or comma seperated list");
-            }
-        }
-
-        request.post(executionUrl, {
-            body: {
-                trigger: (action.params.TRIGGER || 'Started by map-executer plugin'),
-                agents: agents,
-                mergeConfig: action.params.MERGE_CONFIG,
-                config: action.params.CONFIG
-            },
-            headers:{
-                authorization:settings.TOKEN
-            },
-            json : true
-        }, function (error, response, body) {
-            if (error || response.statusCode !== 200) {
-                return reject(body || error);
-            }
-            console.log("You can view the results of the map by entering");
-            console.log(`/maps/${action.params.MAP}/results`);
-            return resolve(body);
-        });
-
-    });
+        console.log("You can view the results of the map by entering");
+        console.log(`/maps/${action.params.MAP}/results`);
+        return resolve(body);
+      }
+    );
+  });
 }
 
 /**
  * In order to be able to support multiple agent versions (<1.3.0, >=1.3.0)
  */
-function getServerUrl(){
-    let server_url;
-    try{
-        const env = require('../../../core/src/environment/environment');
-        server_url = env.server_url
-    } catch(err) {
-        server_url = process.env.SERVER_URL;
-    }
+function getServerUrl() {
+  let server_url;
+  try {
+    const env = require("../../../core/src/environment/environment");
+    server_url = env.server_url;
+  } catch (err) {
+    server_url = process.env.SERVER_URL;
+  }
 
-    if(server_url) return server_url;
-    throw "Could not determine Kaholo server URL";
+  if (server_url) return server_url;
+  throw "Could not determine Kaholo server URL";
 }
 
 module.exports = {
-    executeMap : executeMap
-}
+  executeMap: executeMap,
+};
